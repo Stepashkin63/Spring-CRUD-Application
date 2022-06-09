4f2d469f2597959ea69113249afa622f9a2f036e
Stepashkin63 <spetc_kill@mail.ru>
2022-06-09 19:06:16 +0400

    add SpringCRUDApplication

---
+++ src/main/java/ru/demakin/springcourse/DAO/PersonDAO.java
@@ -0,0 +1,50 @@
+package ru.demakin.springcourse.DAO;
+
+import org.springframework.stereotype.Component;
+import ru.demakin.springcourse.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Stepa", 38, "stepa@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Natasha", 38, "natasha@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Nastya", 14, "nastya@mail.ru"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream()
+                .filter(person -> person.getId() == id)
+                .findAny()
+                .orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+
+    public void update(int id, Person updatedPerson) {
+        Person personToBeUpdated = show(id);
+
+        personToBeUpdated.setName(updatedPerson.getName());
+        personToBeUpdated.setAge(updatedPerson.getAge());
+        personToBeUpdated.setEmail(updatedPerson.getEmail());
+    }
+
+    public void delete(int id) {
+        people.removeIf(p -> p.getId() == id);
+    }
+}
